fd13fed283c812c067e9ac2c7f3c51ee50258f6a
Ahmed ramadan <ramadan@campagency.net>
2018-08-08 15:57:44 +0200

    new changes

--- travels/serializers.py
+++ travels/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-from accounts.serializers import UserSerializer
+# from accounts.serializers import UserSerializer
 from .models import Travel, TravelImage, TravelVideo
+from accounts.models import User
+
+from django.conf import settings
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -21,25 +24,27 @@ class VideoSerializer(serializers.ModelSerializer):
         fields = ('video',)
 
 
-class TravelSerializer(serializers.ModelSerializer):
+class TravelSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer to map the Model instance into JSON format."""
-    users = UserSerializer(many=True)
+    users = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
     images = ImageSerializer(many=True)
     videos = VideoSerializer(many=True)
+    api_key = serializers.Field(source=settings.STRIPE_PUBLIC_KEY)
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Travel
         fields = (
-            'users', 'title', 'description', 'price', 'has_offer', 'offer', 'start', 'end',
-            'limit', 'location', 'images', 'videos',
+            'id', 'url', 'title', 'description', 'price', 'has_offer', 'offer', 'start', 'end', 'limit',
+            'users', 'location', 'duration', 'price_in_cents', 'images', 'videos', 'api_key',
         )
-        read_only_fields = ('duration',)
+        read_only_fields = ('id', 'duration', 'price_in_cents', 'api_key')
 
     def create(self, validated_data):
         imgs = validated_data.pop('images')
         vids = validated_data.pop('videos')
         validated_data.pop('users')
+        validated_data['available_seats'] = validated_data['limit']
         travel = Travel.objects.create(**validated_data)
         for img in imgs:
             TravelImage.objects.create(travel=travel, **img)
