60480afb797b90c7b959e4e54c09ee667f055ab6
Ahmed ramadan <ramadan@campagency.net>
2018-08-12 19:28:14 +0200

    new changes

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from accounts.serializers import UserSerializer
 from .models import Article, ArticleVideo
+from accounts.models import User
 
 
 class VideoSerializer(serializers.ModelSerializer):
@@ -12,18 +13,27 @@ class VideoSerializer(serializers.ModelSerializer):
         fields = ('video',)
 
 
+class ArticleUserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="accounts:user-detail")
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'first_name', 'last_name', 'biography', 'avatar',)
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
-    user = UserSerializer(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name="articles:article-detail")
+    user = ArticleUserSerializer(many=False, read_only=True)
     videos = VideoSerializer(many=True)
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Article
         fields = (
-            'id', 'title', 'description', 'slug', 'cover_img', 'user', 'videos', 'created', 'updated',  # 'url',
+            'url', 'title', 'description', 'slug', 'cover_img', 'user', 'videos', 'created', 'updated',
         )
-        read_only_fields = ('id', 'created',)
+        read_only_fields = ('created',)
 
     def create(self, validated_data):
         vids = validated_data.pop('videos')
